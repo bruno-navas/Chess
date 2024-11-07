cf1d4d4f9c7a8aee17e97e1024ddbbb9477d3fab
Bruno Navas <bruno.navas@usp.br>
2024-11-07 20:07:03 -0300

    Movimento parcial dos peoes

--- main.cpp
+++ main.cpp
@@ -8,21 +8,21 @@ int main() {
     _setmode(_fileno(stdout), _O_U16TEXT);
 
     Tabuleiro t;
-    t.imprimir_Tabuleiro();
 
     bool vez = true;
     int x, x_;
     wchar_t y, y_;
 
     while(true) {
+        t.imprimir_Tabuleiro();
         wcout << endl << L"Escolha a peca a ser movida:";
         wcin >> y >> x;
         wcout << endl << L"Escolha a jogada a ser feita:";
         wcin >> y_ >> x_;
+        wcout << endl;
 
         if(t.jogada(y, x, y_, x_, vez)) {
-            vez = false;
+            vez = !vez;
         }
-        t.imprimir_Tabuleiro();
     }
 }
